fd3495fa2b42bbc18fdd15c1e1ea3b72e0af3b6e
rkwadriga <dmitry.kushneriov@obreey-products.com>
2023-06-19 14:55:50 +0300

    Install symfony/validator; Create forms template "form/fields.html.twig"; Validate the "MicroPost" from

--- src/Entity/MicroPost.php
+++ src/Entity/MicroPost.php
@@ -5,19 +5,30 @@ namespace App\Entity;
 use DateTimeImmutable;
 use App\Repository\MicroPostRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: MicroPostRepository::class)]
 class MicroPost
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
+    #[
+        ORM\Id,
+        ORM\GeneratedValue,
+        ORM\Column
+    ]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[
+        ORM\Column(length: 255),
+        Assert\NotBlank(),
+        Assert\Length(min: 3, max: 255)
+    ]
     private ?string $title = null;
 
-    #[ORM\Column(length: 500)]
+    #[
+        ORM\Column(length: 500),
+        Assert\NotBlank(),
+        Assert\Length(min: 3, max: 500)
+    ]
     private ?string $text = null;
 
     #[ORM\Column]
